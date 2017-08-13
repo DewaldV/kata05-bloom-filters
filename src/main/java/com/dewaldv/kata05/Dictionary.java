9eeb2f222510d32d1cb2667642243965b919b18b
DewaldV <dewald.viljoen@gmail.com>
2017-08-13 19:22:24 +0100

    Implemented kata05.

---
+++ src/main/java/com/dewaldv/kata05/Dictionary.java
@@ -0,0 +1,29 @@
+package com.dewaldv.kata05;
+
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+
+public class Dictionary {
+
+    private final boolean[] bitmap;
+
+    public Dictionary() {
+        this(0xfffff);
+    }
+
+    public Dictionary(int mapSize) {
+        this.bitmap = new boolean[mapSize];
+    }
+
+    public void add(String element) {
+        bitmap[elementIdx(element)] = true;
+    }
+
+    public boolean contains(String element) {
+        return bitmap[elementIdx(element)];
+    }
+
+    private int elementIdx(String element) {
+        String md5sum = md5Hex(element);
+        return Integer.parseInt(md5sum.substring(5, 10), 16);
+    }
+}
---
+++ src/test/java/com/dewaldv/kata05/DictionaryTest.java
@@ -0,0 +1,51 @@
+package com.dewaldv.kata05;
+
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DictionaryTest {
+
+    @Test
+    public void shouldAddItemToSet() {
+        Dictionary set = new Dictionary();
+
+        set.add("test");
+        boolean contains = set.contains("test");
+
+        assertThat(contains, is(true));
+    }
+
+    @Test
+    public void shouldNotReportItemInSetWhenNotAdded() {
+        Dictionary set = new Dictionary();
+        set.add("test1");
+        set.add("test2");
+
+        assertThat(set.contains("test1"), is(true));
+        assertThat(set.contains("test2"), is(true));
+        assertThat(set.contains("test3"), is(false));
+    }
+
+    @Test
+    public void shouldCheckWordsInDictionary() throws Exception {
+        String unixDict = "/usr/share/dict/words";
+
+        Dictionary dictionary = new Dictionary();
+
+        Stream<String> lines = Files.lines(Paths.get(unixDict));
+        lines.forEach(dictionary::add);
+
+        assertThat(dictionary.contains("hyper"), is(true));
+        assertThat(dictionary.contains("special"), is(true));
+        assertThat(dictionary.contains("wonderful"), is(true));
+        assertThat(dictionary.contains("blaargh1"), is(false));
+        assertThat(dictionary.contains("arrrgh"), is(false));
+        assertThat(dictionary.contains("definitelynotinthedictionary"), is(false));
+    }
+}
